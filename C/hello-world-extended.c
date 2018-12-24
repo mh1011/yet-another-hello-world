02abbecd06f286b81556875c09b0ec4a63e3000f
Muhammad Habibullah <muhammadhabibullah1011@gmail.com>
2018-12-24 12:09:08 +0600

    Added ASM

---
+++ C/hello-world-extended.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+
+/*  
+
+int main ()
+{
+	int num_loop, i;
+	printf("Number of Loop Cycle: ");
+	scanf("%d", &num_loop);
+	
+	for (i=1; i<=num_loop; i++)
+	{
+		printf("Hello World\n");
+	}
+
+
+
+	return 0;
+}
+
+*/
+
+
+/* */
+//int menu (int choice)
+int main()
+{
+	printf("Welcome To Selection Menu ");
+	printf("Enter Text: ");	
+	printf("Number of Loop Cycle");
+	printf("Enter Text ");
+	printf("");
+	printf("");
+
+
+
+	return 0;
+}	
+
+
+
+
+/* */
